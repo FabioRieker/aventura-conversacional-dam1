1e60a87bd99a4ffc863889cc01e6d26ee3ffc9f0
FabioRieker <faabiioo05@gmail.com>
2025-11-02 19:25:46 +0100

    Proyecto actualizado

--- proyectoHarryPotter/src/proyectoHarryPotter/ProyectoHP.java
+++ proyectoHarryPotter/src/proyectoHarryPotter/ProyectoHP.java
@@ -5,6 +5,10 @@ public class ProyectoHP {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola puto");
+		System.out.println("Hola mike");
+		
+		
+		
 	}
 
 }
