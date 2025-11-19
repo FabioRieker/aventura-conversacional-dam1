ede1140f9f151ed27fd798bd167a72621d487788
FabioRieker <faabiioo05@gmail.com>
2025-11-19 17:59:01 +0100

    Fabio:Actualizo el codigo principal a modo de prueba

--- proyectoHarryPotter/src/proyectoHarryPotter/ProyectoHP.java
+++ proyectoHarryPotter/src/proyectoHarryPotter/ProyectoHP.java
@@ -4,8 +4,9 @@ public class ProyectoHP {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola puto");
-		System.out.println("Hola mike");
+		System.out.println("╔══════════════════════════════════════════════════╗");
+		System.out.println("║     	Aventura conversacional Harry Potter       ║");
+		System.out.println("╚══════════════════════════════════════════════════╝");
 		
 		
 		
