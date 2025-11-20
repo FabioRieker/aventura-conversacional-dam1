fcc8467899711b2ef35b16c9e4d3381b8608d027
Mateo Garcia <garciacarrascomat@gmail.com>
2025-11-20 14:06:44 +0100

    Mateo: Segunda prueba en ProyectoHP.java

--- proyectoHarryPotter/src/proyectoHarryPotter/ProyectoHP.java
+++ proyectoHarryPotter/src/proyectoHarryPotter/ProyectoHP.java
@@ -3,6 +3,7 @@ package proyectoHarryPotter;
 public class ProyectoHP {
 
 	public static void main(String[] args) {
+		System.out.println("Segunda prueba");
 		// TODO Auto-generated method stub
 		System.out.println("╔══════════════════════════════════════════════════════════════════════╗");
 		System.out.println(
